d5cb8cab0c5ff9456fc3cd80c92c82e4b3a95241
Benedict Tan <bendamian2012@gmail.com>
2023-10-02 07:24:43 -0700

    fixed file upload

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -6,7 +6,7 @@ function ResumeSection(){
         <div id="resumeheading" className="resumesection">
             <h2>RESUME</h2>
             <div className="content">
-                <a href="/Benedict_Tan_Coop_Resume.png" download="Benedict_Tan_Resume.pdf" className="download-button">
+                <a href="/Benedict_Tan_Coop_Resume.pdf" download="Benedict_Tan_Resume.pdf" className="download-button">
                     <p className='interactable'>Download my resume:</p>
                     <svg className='icon' xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 -960 960 960" width="64"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
                 </a>
